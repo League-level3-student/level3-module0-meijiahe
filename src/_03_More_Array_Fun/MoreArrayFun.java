c04b6303383cf12e08bf19e6e0f669dc527cf145
Meiyi <hemeiyi0225@gmail.com>
2020-06-21 16:00:49 -0700

    MoreArrayFun

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,20 +4,41 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
-	
-	
-	
+	public static void main(String[] args) {
+		//String [] stringa= {"aa","bb","cc"};
+		//arraystring(stringa);
+		
+	}
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
-	
+	//public static String arraystring(String [] values) {	
+	//String arraystring="";
+		//for (int i = 0;i<values.length;i++) {
+			//System.out.println(values[i]);
+		//}
+		//return arraystring;
+		
+	//}
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	
+	//public static String arraystring (String []pp) {
+		//String arraystring="";
+		//for(int i = pp.length-1;i>=0;i--) {
+		//	System.out.println(pp[i]);
+		//}
+		//return arraystring;
+	//}
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+	//public static String arraystring(String [] values) {	
+		//String arraystring="";
+			//for (int i = 0;i<values.length;i+=2) {
+				//System.out.println(values[i]);
+			//}
+		//	return arraystring;
+			
+		//}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
